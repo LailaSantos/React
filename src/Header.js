e4a5f1a624c9cee788d1a646a8f5006047bae75c
Laila Santos <lailaroberta100@gmail.com>
2019-11-19 17:36:37 -0200

    pag atualizada

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,8 @@
 import React from "react";
 
-const Header = (props) => <div>
+const Header = () => <div>
 	<h1>VAI TODO MUNDO</h1>
-
 	<img src="https://mundosublinhado.files.wordpress.com/2017/07/marvin.jpg?w=375&h=452" />
-
 	<h1>COMER MERDA</h1>
 
 
